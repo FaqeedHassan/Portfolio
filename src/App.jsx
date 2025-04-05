c498fe42fadd30687a2680123553ff8bae4b9445
ali abbas <2021se54@student.uet.edu.pk>
2025-04-05 15:24:53 +0500

    Portfolio completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Navbar from './Components/Navbar/Navbar';
 import Hero from './Components/Hero/Hero';
 import About from './Components/About/About';
 import Experience from './Components/Experience/Experience';
+import Projects from './Components/Projects/Projects';
+import Contact from './Components/Contact/Contact';
 
 const App = () => {
   return (
@@ -12,6 +14,8 @@ const App = () => {
       <Hero />
       <About />
       <Experience />
+      <Projects />
+      <Contact />
     </div>
   )
 }
